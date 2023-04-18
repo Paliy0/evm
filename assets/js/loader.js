756a595a545b2cfd9ef15ed6e752edc20017df48
paliy0 <pabloguliasprats@gmail.com>
2023-04-18 14:33:21 +0200

    cloudinary and optimization

---
+++ assets/js/loader.js
@@ -0,0 +1,8 @@
+const cloudinary = require("cloudinary").v2;
+
+// Configuration
+cloudinary.config({
+  cloud_name: "dehswyyl9",
+  api_key: "171771965358752",
+  api_secret: "wYZc49tts1Ipn4_QF0XuuqzdJR8",
+});
